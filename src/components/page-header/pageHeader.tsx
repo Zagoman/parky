a5590972d47156ec0580bff4ae84444f571f8b15
Fryderyk Boncler <f.relcnob@gmail.com>
2023-05-11 14:39:34 +0200

    chore: updated with dev branch

--- src/components/page-header/pageHeader.tsx
+++ src/components/page-header/pageHeader.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import logoImage from "../../../public/parky-logo-blue.svg";
 import menuImage from "../../../public/icon/menu.svg";
 import pageInfoImage from "../../../public/icon/page-info.svg";
+import closeImage from "../../../public/icon/close.svg";
 
 export const PageHeader: () => JSX.Element = () => {
   const [menuVisibility, setMenuVisiblity] = useState<boolean>(false);
   const logo = logoImage as string;
   const menuIcon = menuImage as string;
   const pageInfoIcon = pageInfoImage as string;
+  const closeIcon = closeImage as string;
 
   return (
     <header className={styles.pageHeader}>
@@ -35,7 +37,11 @@ export const PageHeader: () => JSX.Element = () => {
           menuVisibility ? setMenuVisiblity(false) : setMenuVisiblity(true);
         }}
       >
-        <Image src={menuIcon} alt="menu" className={styles.menuIcon} />
+        <Image
+          src={menuVisibility ? closeIcon : menuIcon}
+          alt="menu"
+          className={styles.menuIcon}
+        />
       </div>
       <nav className={menuVisibility ? styles.visible : styles.hidden}>
         <ul>
