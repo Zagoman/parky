517aba870b16487c0bb050e3712da1b8a9371f0d
LucasZago <lzs@signifly.com>
2023-05-11 23:48:21 +0200

    feat: add zod error messages and toast

--- src/components/CreateProfile/CreateProfile.tsx
+++ src/components/CreateProfile/CreateProfile.tsx
@@ -1,58 +1,104 @@
-import { useState } from "react"
-import styles from "./CreateProfile.module.scss";
-import { InputField } from "../FormElements/InputField/InputField";
-import { Checkbox } from "../FormElements/CheckBox/Checkbox";
+import { useState } from 'react'
+import styles from './CreateProfile.module.scss'
+import { InputField } from '../FormElements/InputField/InputField'
+import { Checkbox } from '../FormElements/CheckBox/Checkbox'
+import { api } from '~/utils/api'
+import { toast } from 'react-hot-toast'
 const CreateProfile: React.FC = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [username, setUsername] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [isDriver, setIsDriver] = useState<boolean>(false);
-    const [isOwner, setIsOwner] = useState<boolean>(false);
-    return <>
-        <div className={styles.form}>
-            <InputField
-                name="firstname"
-                label="First name"
-                inputType="text"
-                value={firstName}
-                onChange={setFirstName}
-                placeholder="First Name" />
-            <InputField
-                name="lastName"
-                label="Last name"
-                inputType="text"
-                value={lastName}
-                onChange={setLastName}
-                placeholder="Last Name" />
-            <InputField
-                name="username"
-                label="Username"
-                inputType="text"
-                value={username}
-                onChange={setUsername}
-                placeholder="Username" />
-            <InputField
-                name="phoneNumber"
-                label="Phone number"
-                inputType="text"
-                placeholder="Phone number"
-                value={phoneNumber}
-                onChange={setPhoneNumber}
-            />
-            <Checkbox
-                name="isDriver"
-                label="Do you drive?"
-                checked={isDriver}
-                onChange={setIsDriver}
-            />
-            <Checkbox
-                name="isOwner"
-                label="Do you own a parking space?"
-                checked={isOwner}
-                onChange={setIsOwner}
-            />
-        </div>
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [isDriver, setIsDriver] = useState<boolean>(false)
+  const [isOwner, setIsOwner] = useState<boolean>(false)
+  const { mutate, isLoading: isPosting } = api.profile.create.useMutation({
+    onSuccess: () => {
+      setFirstName('')
+      setLastName('')
+      setIsOwner(false)
+      setIsDriver(false)
+      setPhoneNumber('')
+      setUsername('')
+    },
+    onError: (e) => {
+      const errorMessages = e.data?.zodError?.fieldErrors
+      if (errorMessages) {
+        for (const key in errorMessages) {
+          if (errorMessages[key] !== undefined) {
+            toast.error(errorMessages[key]?.at(0) as string)
+          }
+        }
+        return
+      }
+      toast.error('Invalid submission, please try again')
+    },
+  })
+
+  const submitForm = () => {
+    const profile = mutate({
+      firstName,
+      lastName,
+      isOwner,
+      isDriver,
+      phoneNumber,
+      username,
+    })
+    console.log(profile)
+  }
+
+  return (
+    <>
+      <div className={styles.form}>
+        <InputField
+          name='firstname'
+          label='First name'
+          inputType='text'
+          value={firstName}
+          onChange={setFirstName}
+          placeholder='First Name'
+        />
+        <InputField
+          name='lastName'
+          label='Last name'
+          inputType='text'
+          value={lastName}
+          onChange={setLastName}
+          placeholder='Last Name'
+        />
+        <InputField
+          name='username'
+          label='Username'
+          inputType='text'
+          value={username}
+          onChange={setUsername}
+          placeholder='Username'
+        />
+        <InputField
+          name='phoneNumber'
+          label='Phone number'
+          inputType='text'
+          placeholder='Phone number'
+          value={phoneNumber}
+          onChange={setPhoneNumber}
+        />
+        <Checkbox
+          name='isDriver'
+          label='Do you drive?'
+          checked={isDriver}
+          onChange={setIsDriver}
+        />
+        <Checkbox
+          name='isOwner'
+          label='Do you own a parking space?'
+          checked={isOwner}
+          onChange={setIsOwner}
+        />
+        <button onClick={submitForm} disabled={isPosting}>
+          {' '}
+          Create your profile{' '}
+        </button>
+      </div>
     </>
+  )
 }
 export default CreateProfile
--- src/components/FormElements/InputField/InputField.tsx
+++ src/components/FormElements/InputField/InputField.tsx
@@ -1,17 +1,29 @@
+import styles from './InputField.module.scss'
 interface InputFieldProps {
-    inputType: string;
-    value: string;
-    label: string;
-    name: string;
-    placeholder: string;
-    onChange: (value:string) => void
+  inputType: string
+  value: string
+  label: string
+  name: string
+  placeholder: string
+  onChange: (value: string) => void
 }
 export const InputField: React.FC<InputFieldProps> = (props) => {
-    const { inputType, value, label, name, placeholder, onChange } = props
-    return <div>
-        <label htmlFor={name}>
-            {label}
-            <input type={inputType} value={value} id={name} name={name} placeholder={placeholder} onChange={(e:React.ChangeEvent<HTMLInputElement>) => onChange(e)} />
-        </label>
+  const { inputType, value, label, name, placeholder, onChange } = props
+  return (
+    <div className={styles.inputWrapper}>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+        <input
+          type={inputType}
+          value={value}
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            onChange(e.target.value)
+          }
+        />
+      </label>
     </div>
+  )
 }
