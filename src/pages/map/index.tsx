7495ae0cab7ac981f36b67ea1df9f68b0eeec26c
Fryderyk Boncler <f.relcnob@gmail.com>
2023-05-13 16:06:11 +0200

    fix: adjusted dashboard behavior

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,9 +1,41 @@
 import { type NextPage } from "next";
 import { PageHeader } from "../../components/pageHeader/pageHeader";
-import { DashboardHeader } from "~/components/dashboardHeader/dashboardHeader";
-
+import { DashboardWrapper } from "~/components/dashboardWrapper/dashboardWrapper";
+import { UiBox } from "../../components/uiBox/uiBox";
+import { Button } from "~/components/button/button";
 const Map: NextPage = () => {
-  return <DashboardHeader />;
+  return (
+    <>
+      <DashboardWrapper>
+        <div>
+          <UiBox>
+            <Button
+              onClick={() => console.log("test")}
+              type="primary"
+              text="Primary button"
+            />
+            <Button
+              onClick={() => console.log("test")}
+              type="secondary"
+              text="Secondary button"
+            />
+          </UiBox>
+          <UiBox>
+            <Button
+              onClick={() => console.log("test")}
+              type="primary"
+              text="Primary button"
+            />
+            <Button
+              onClick={() => console.log("test")}
+              type="secondary"
+              text="Secondary button"
+            />
+          </UiBox>
+        </div>
+      </DashboardWrapper>
+    </>
+  );
 };
 
 export default Map;
