7cc87266b30811b80e27e5b659a5c9ebab1fed22
Fryderyk Boncler <f.relcnob@gmail.com>
2023-05-21 18:09:42 +0200

    feat: added log in and log out handling to pageheader

--- src/components/pageHeader/pageHeader.tsx
+++ src/components/pageHeader/pageHeader.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import styles from "./pageHeader.module.scss";
 import { useState } from "react";
 import Image from "next/image";
@@ -5,6 +6,9 @@ import menuImage from "../../../public/icon/menu.svg";
 import pageInfoImage from "../../../public/icon/page-info.svg";
 import closeImage from "../../../public/icon/close.svg";
 import PakyLogoBlue from "../../../public/parky-logo-blue.svg";
+import { SignOutButton, useUser, SignIn, SignUp } from "@clerk/nextjs";
+
+import Link from "next/link";
 
 type headerProps = {
   children: JSX.Element;
@@ -20,10 +24,14 @@ export const PageHeader = ({
   const [menuVisibility, setMenuVisiblity] = useState<boolean>(false);
   const [secondaryMenuVisibility, setSecondaryMenuVisiblity] =
     useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalContents, setModalContents] = useState<JSX.Element>();
   const menuIcon = menuImage as string;
   const pageInfoIcon = pageInfoImage as string;
   const closeIcon = closeImage as string;
   const parkyLogo = PakyLogoBlue as string;
+  const user = useUser();
+
   return (
     <>
       <header className={styles.pageHeader}>
@@ -58,13 +66,15 @@ export const PageHeader = ({
         )}
 
         <div className={styles.pageHeaderLogoWrapper}>
-          <Image
-            src={parkyLogo}
-            width="200"
-            height="50"
-            alt="parky logo"
-            className={styles.pageHeaderLogo}
-          />
+          <Link href="/">
+            <Image
+              src={parkyLogo}
+              width="200"
+              height="50"
+              alt="parky logo"
+              className={styles.pageHeaderLogo}
+            />
+          </Link>
         </div>
         <div
           className={styles.mobileButton}
@@ -81,17 +91,70 @@ export const PageHeader = ({
         </div>
         <nav className={menuVisibility ? styles.visible : styles.hidden}>
           <ul>
-            <li className={styles.mobileButton}>Home</li>
-            <li>Find parking</li>
+            <li className={styles.mobileButton}>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/map">Find parking</Link>
+            </li>
             <li>How it works</li>
             <li>Help</li>
-            <li>Log in</li>
-            <li>Sign up</li>
+            {!user.isSignedIn ? (
+              <>
+                <li
+                  onClick={() => {
+                    setModalContents(<SignIn />);
+                    setIsModalVisible(true);
+                  }}
+                >
+                  Sign in
+                </li>
+                <li
+                  onClick={() => {
+                    setModalContents(<SignUp />);
+                    setIsModalVisible(true);
+                  }}
+                >
+                  Sign up
+                </li>
+              </>
+            ) : (
+              <li>
+                <SignOutButton />
+              </li>
+            )}
             <li className={styles.pageHeaderCTA}>Rent your parking</li>
           </ul>
         </nav>
       </header>
       <section className={secondaryMenu ? styles.contentWrapper : ""}>
+        <div
+          className={
+            isModalVisible
+              ? `${styles.modal} `
+              : `${styles.modal} ${styles.modalHidden}`
+          }
+        >
+          <div className={styles.modalWrapper}>
+            <span
+              className={styles.modalWrapperClose}
+              onClick={() => {
+                setIsModalVisible(false);
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="36"
+                height="36"
+                fill="#566777"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+              </svg>
+            </span>
+            {modalContents}
+          </div>
+        </div>
         {children}
       </section>
     </>
