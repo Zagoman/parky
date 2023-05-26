565f1aa7d9450384364e86c45cbf2fcd5d04b25f
Fryderyk Boncler <f.relcnob@gmail.com>
2023-05-26 21:11:28 +0200

    feat: added sorting

--- src/pages/account/bookings/index.tsx
+++ src/pages/account/bookings/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import styles from "./index.module.scss";
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
@@ -16,6 +17,9 @@ import Image from "next/image";
 const Home: NextPage = () => {
   const [userId, setUserId] = useState("");
   const [activeView, setActiveView] = useState("");
+  const [sortingValue, setSortingValue] = useState("default");
+  const [bookingItems, setBookingItems] = useState<BookingElement[]>([]);
+  const [isSortingVisible, setIsSortingVisible] = useState(false);
   const user = useUser();
 
   const { data: userData, refetch: refetchUser } =
@@ -40,6 +44,52 @@ const Home: NextPage = () => {
       void refetchBookings();
     }
   }, [user.isLoaded]);
+
+  useEffect(() => {
+    if (userBookingData?.length) {
+      setBookingItems(userBookingData);
+    }
+  }, [userBookingData]);
+
+  useEffect(() => {
+    if (userBookingData?.length) {
+      if (sortingValue === "default") {
+        setBookingItems(userBookingData);
+      } else if (sortingValue === "newest") {
+        const sorted = [...userBookingData];
+        setBookingItems(
+          sorted.sort((a, b) => {
+            return new Date(b.start).getTime() - new Date(a.start).getTime();
+          })
+        );
+      } else if (sortingValue === "oldest") {
+        const sorted = [...userBookingData];
+        setBookingItems(
+          sorted.sort((a, b) => {
+            return new Date(a.start).getTime() - new Date(b.start).getTime();
+          })
+        );
+      } else if (sortingValue === "cost") {
+        const sorted = [...userBookingData];
+        setBookingItems(
+          sorted.sort((a, b) => {
+            return b.price - a.price;
+          })
+        );
+      } else if (sortingValue === "duration") {
+        const sorted = [...userBookingData];
+        setBookingItems(
+          sorted.sort((a, b) => {
+            return (
+              b.end.getHours() -
+              b.start.getHours() -
+              (a.end.getHours() - a.start.getHours())
+            );
+          })
+        );
+      }
+    }
+  }, [sortingValue]);
 
   return (
     <DashboardWrapper
@@ -68,11 +118,45 @@ const Home: NextPage = () => {
               )}
             </div>
             <div className={styles.filters}>
-              <Image src={filterIcon} alt="filtering icon" />
-              <span>
-                <p>Sort</p>
-
-                <p>Filter</p>
+              <Image
+                src={filterIcon}
+                alt="filtering icon"
+                onClick={() => {
+                  isSortingVisible
+                    ? setIsSortingVisible(false)
+                    : setIsSortingVisible(true);
+                }}
+              />
+              <span
+                className={
+                  isSortingVisible
+                    ? styles.sortingWrapper
+                    : `${styles.sortingWrapper} ${styles.sortingHidden}`
+                }
+              >
+                <select
+                  className={styles.selectInput}
+                  value={sortingValue}
+                  onChange={(e) => {
+                    setSortingValue(e.target.value);
+                  }}
+                >
+                  <option value="default" className={styles.selectOption}>
+                    Default sorting
+                  </option>
+                  <option value="newest" className={styles.selectOption}>
+                    Sort by newest
+                  </option>
+                  <option value="oldest" className={styles.selectOption}>
+                    Sort by oldest
+                  </option>
+                  <option value="cost" className={styles.selectOption}>
+                    Sort by cost
+                  </option>
+                  <option value="duration" className={styles.selectOption}>
+                    Sort by duration
+                  </option>
+                </select>
               </span>
             </div>
           </div>
@@ -84,8 +168,8 @@ const Home: NextPage = () => {
             <p>Details</p>
           </div>
           <ul>
-            {userBookingData && !areBookingsLoading
-              ? userBookingData.map((booking: BookingElement) => (
+            {bookingItems && !areBookingsLoading
+              ? bookingItems.map((booking: BookingElement) => (
                   <BookingItem key={booking.id} booking={booking} />
                 ))
               : areBookingsLoading
